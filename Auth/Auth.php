23b639436a3080baf2e2178eb16de443c1eaab0e
bukatja567 <bukatja567@mail.ru>
2019-04-11 00:48:35 +0300

    CCNA

---
+++ Auth/Auth.php
@@ -0,0 +1,10 @@
+<?php
+include 'Auth_ID.php';
+
+function Auth($user_id)
+    {
+    foreach (ROOT as $i) //ROOT - группа пользователей, прописывается в Auth_ID.php
+        if ($user_id == $i) return "root";
+    return "student";
+    }
+?>
